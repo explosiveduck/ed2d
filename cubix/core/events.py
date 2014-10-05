cb046bea862568b2aef3cf57b83cc930b96cf3c2
Matthew Sitton <matthewsitton@gmail.com>
2014-10-04 22:43:31 -0500

    Finish implementing the basic structure of the game.

--- cubix/core/events.py
+++ cubix/core/events.py
@@ -1,8 +1,57 @@
+import ctypes as ct
+
 import sdl2 as sdl
 
 class Events(object):
+    ''' Handles Event stuff... '''
     def __init__(self):
-        pass
+        sdl.SDL_InitSubSystem(sdl.SDL_INIT_EVENTS)
+
+        self.listeners = []
+
+    def add_listener(self, listener):
+        '''
+        Add an event processing listener.
+        An event listener is just a function that accepts 2 arguments.
+        '''
+        self.listeners.append(listener)
+
+    def remove_listener(self, listener):
+        ''' Remove specified event listener '''
+        if not listener in self.listeners:
+            self.listener.remove()
+
+    def broadcast_event(self, event, args):
+        ''' Send out an event to all event listeners '''
+        for listener in self.listeners:
+            listener(event, args)
 
     def process(self):
-        pass
+        '''
+        Processes the events polled from sdl.
+        Custom events might be a possiblility if we need them.
+        '''
+        
+        event = sdl.SDL_Event()
+
+        while sdl.SDL_PollEvent(ct.byref(event)):
+
+            eventName = None
+            data = None
+
+            if event.type == sdl.SDL_QUIT:
+                eventName = 'quit'
+                data = tuple()
+            if event.type == sdl.SDL_WINDOWEVENT:
+                winEvent = event.window.event
+                # For now this will only support one window
+                # If we want two later on then we can do it then.
+                if winEvent == sdl.SDL_WINDOWEVENT_CLOSE:
+                    eventName = 'window_close'
+                    data = tuple()
+            else:
+                # Will add more event types later
+                pass
+
+            if not eventName is None:
+                self.broadcast_event(eventName, data)
--- cubix/game/gamemanager.py
+++ cubix/game/gamemanager.py
@@ -1,10 +1,15 @@
+import ctypes as ct
 import sdl2 as sdl
-from cubix.core.opengl import gl
+
 from cubix.core import window
 from cubix.core import events
 from cubix.core import context
+from cubix.core.opengl import gl
+from cubix.core.opengl import pgl
+
 
 class GameManager(object):
+    ''' Entry point into the game, and manages the game in general '''
     def __init__(self):
 
         self.width = 800
@@ -12,23 +17,40 @@ class GameManager(object):
         self.title = "Cubix"
         self.running = False
 
+        window.init_video()
+
         self.events = events.Events()
         self.window = window.Window(self.title, self.width, self.height, False)
         self.context = context.Context(3, 3, 2)
         self.context.window = self.window
 
+        self.events.add_listener(self.process_event)
+
+        gl.init()
+        major = pgl.glGetInteger(gl.GL_MAJOR_VERSION)
+        minor = pgl.glGetInteger(gl.GL_MINOR_VERSION)
+        print ('OpenGL Version: {}.{}'.format(major, minor))
+
+    def process_event(self, event, data):
+        if event == 'quit' or event == 'window_close':
+            self.running = False
+
     def update(self):
         pass
 
     def render(self):
-        pass
+        gl.glClearColor(0.5, 0.5, 0.5, 1.0)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
     def do_run(self):
+        ''' Process a single loop '''
+        self.events.process()
         self.update()
         self.render()
         self.window.flip()
 
     def run(self):
+        ''' Called from launcher doesnt exit until the game is quit '''
         self.running = True
         while self.running:
             self.do_run()
