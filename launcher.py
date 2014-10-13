60da80b5f09af88c0efcef87e667069c0dfae107
Matthew Sitton <matthewsitton@gmail.com>
2014-10-13 12:52:46 -0500

    Set default path for sdl2 dlls to deps folder.

--- launcher.py
+++ launcher.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+import platform
+import os
+from cubix.core import files
+# Set PYSDL2_DLL_PATH to deps folder
+if platform.system() == 'Windows':
+    os.environ['PYSDL2_DLL_PATH'] = files.resolve_path('deps')
 from cubix.game import gamemanager
 
 if __name__ == '__main__':
