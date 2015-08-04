a81ed910e03c9128aec74448b5030ec04ca83006
Matthew Sitton <matthewsitton@gmail.com>
2015-08-04 11:44:26 -0500

    Add some docstrings in files.py

--- ed2d/files.py
+++ ed2d/files.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 def get_path():
-
+    ''' get the current path '''
     path0 = os.path.realpath(sys.path[0])
     path1 = os.path.realpath(sys.path[1])
 
@@ -14,12 +14,14 @@ def get_path():
         return path0
 
 def resolve_path(*location):
+    '''Resolve path relative to current path.'''
     path = get_path()
     location = (path,) + location
     location = os.sep.join(location)
     return location
 
 def read_file(path):
+    '''Read file and return string'''
     output = ''
     with open(path, 'r') as f:
         output = f.read()
