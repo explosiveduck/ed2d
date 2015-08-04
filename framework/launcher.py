eb1734277c2dc119b8160ebdc2a20acf4eb18c35
Matthew Sitton <matthewsitton@gmail.com>
2015-08-04 11:43:46 -0500

    Some launcher.py simplifications, code will only be used in development environments so there are some edge cases that we dont really need to handle.

--- framework/launcher.py
+++ framework/launcher.py
@@ -3,20 +3,11 @@ import platform
 import os
 import sys
 
-# This function is copied from ed2d.files but since it needs
-# to be accessable before ed2d is imported it has been copied
-# here.
+# This function is simplified from ed2d.files. It needed to be used find ed2d
+# in development environments. However this limited use case allows for a
+# simpler function
 def get_path():
-
-    path0 = os.path.realpath(sys.path[0])
-    path1 = os.path.realpath(sys.path[1])
-
-    pathtest = os.sep.join(path0.split(os.sep)[:-1])
-
-    if pathtest == path1:
-        return path1
-    else:
-        return path0
+    return os.path.realpath(__file__).rsplit(os.path.sep, 1)[0]
 
 try:
     # Test if the ed2d package is found with the python installation.
@@ -26,11 +17,13 @@ try:
 except ImportError:
         # If its not assume its located 1 directory above.
     # Get path of one directory up to get development location.
-    path = os.sep.join(get_path().split(os.sep)[:-1])
+    path = get_path().rsplit(os.sep, 1)[0]
 
     # add location of the ed2d package to the python path so
     # it can be imported.
     sys.path.append(path)
+    
+    import ed2d
 
 from ed2d import files
 from ed2d import cmdargs
@@ -49,8 +42,8 @@ if __name__ == '__main__':
     cmd.parse_args()
 
     game = game()
-    
-    
+
+
     if game:
         gamemanager = __import__(game, fromlist=[game])
     else:
