3548ce42c6636267f554a91ddab55f5f191c2e52
Alex <ale632007@gmail.com>
2015-09-13 01:07:19 -0400

    Fixed the tab and spaces issue with camera.

--- ed2d/camera.py
+++ ed2d/camera.py
@@ -4,137 +4,137 @@ from ed2d.glmath import matrix
 from ed2d.glmath import quaternion # needs implementation 
 
 class Camera(object):
-	def __init__(self):
+    def __init__(self):
 
-		# Camera information
-		self.yAxis = [0.0, 1.0, 0.0]
-		self.position = vector.Vector(3)
-		self.rotation = quaternion.Quaternion()
-		self.sensitivity = 0.05
-		self.aperature = 0
+        # Camera information
+        self.yAxis = [0.0, 1.0, 0.0]
+        self.position = vector.Vector(3)
+        self.rotation = quaternion.Quaternion()
+        self.sensitivity = 0.05
+        self.aperature = 0
 
-		# Camera modifiers 
-		self.cameraRotation = matrix.Matrix(4)
-		self.cameraPosition = matrix.Matrix(4)
-		self.cameraTranslation = matrix.Matrix(4)
+        # Camera modifiers 
+        self.cameraRotation = matrix.Matrix(4)
+        self.cameraPosition = matrix.Matrix(4)
+        self.cameraTranslation = matrix.Matrix(4)
 
-		# These are required for rendering
-		self.perspectiveProj = matrix.Matrix(4)
-		self.orthographicProj = matrix.Matrix(4)
-		self.viewMatrix = matrix.Matrix(4)
-		self.viewMatrixInverse = matrix.Matrix(4)
+        # These are required for rendering
+        self.perspectiveProj = matrix.Matrix(4)
+        self.orthographicProj = matrix.Matrix(4)
+        self.viewMatrix = matrix.Matrix(4)
+        self.viewMatrixInverse = matrix.Matrix(4)
 
-		# Camera will rotate around this point
-		self.originVisiblity = True
-		self.originPosition = [0.0, 0.0, 0.0]
+        # Camera will rotate around this point
+        self.originVisiblity = True
+        self.originPosition = [0.0, 0.0, 0.0]
 
-		# Arcball propertise
-		self.cur_x = 0
-		self.cur_y = 0
-		self.arcball_on = False
+        # Arcball propertise
+        self.cur_x = 0
+        self.cur_y = 0
+        self.arcball_on = False
 
 
-	def perspectiveProj(self, fov, aspect, znear, zfar):
-		self.perspectiveProj = matrix.perspective(fov, aspect, znear, zfar)
+    def perspectiveProj(self, fov, aspect, znear, zfar):
+        self.perspectiveProj = matrix.perspective(fov, aspect, znear, zfar)
 
-	def orthographicProj(self, left, right, bottom, top, znear, zfar):
-		self.orthographicProj = matrix.orthographic(left, right, bottom, top, znear, zfar)
+    def orthographicProj(self, left, right, bottom, top, znear, zfar):
+        self.orthographicProj = matrix.orthographic(left, right, bottom, top, znear, zfar)
 
-	def calcViewMatrix(self):
-		self.cameraRotation = quaternion.toMatrix(quaternion.conjugate(self.rotation))
-		self.cameraPosition = self.position * -1.0
-		self.cameraTranslation.i_translate(self.cameraPosition)
+    def calcViewMatrix(self):
+        self.cameraRotation = quaternion.toMatrix(quaternion.conjugate(self.rotation))
+        self.cameraPosition = self.position * -1.0
+        self.cameraTranslation.i_translate(self.cameraPosition)
 
-		self.viewMatrix = self.cameraTranslation * self.cameraRotation
-		self.viewMatrixInverse = matrix.inverse4(self.viewMatrix)
+        self.viewMatrix = self.cameraTranslation * self.cameraRotation
+        self.viewMatrixInverse = matrix.inverse4(self.viewMatrix)
 
-	def onKeys(self, key, tick):
-		moveAmount = 0.5 * tick
+    def onKeys(self, key, tick):
+        moveAmount = 0.5 * tick
 
-		if 'W' in keys:
-			self.move(self.rotation.getForward(), -moveAmount)
+        if 'W' in keys:
+            self.move(self.rotation.getForward(), -moveAmount)
 
-		if 'S' in keys:
-			self.move(self.rotation.getForward(), moveAmount)
+        if 'S' in keys:
+            self.move(self.rotation.getForward(), moveAmount)
 
-		if 'A' in keys:
-			self.move(self.rotation.getLeft(), moveAmount)
+        if 'A' in keys:
+            self.move(self.rotation.getLeft(), moveAmount)
 
-		if 'D' in keys:
-			self.move(self.rotation.getRight(), moveAmount)
+        if 'D' in keys:
+            self.move(self.rotation.getRight(), moveAmount)
 
-		if 'Q' in keys:
-			self.move(self.rotation.getForward(), math.radians(moveAmount))
+        if 'Q' in keys:
+            self.move(self.rotation.getForward(), math.radians(moveAmount))
 
-		if 'E' in keys:
-			self.move(self.rotation.getForward(), math.radians(-moveAmount))
+        if 'E' in keys:
+            self.move(self.rotation.getForward(), math.radians(-moveAmount))
 
-		if 'UP' in keys:
-			self.move(self.rotation.getUp(), moveAmount)
+        if 'UP' in keys:
+            self.move(self.rotation.getUp(), moveAmount)
 
-		if 'DOWN' in keys:
-			self.movie(self.rotation.getUp(), -moveAmount)
+        if 'DOWN' in keys:
+            self.movie(self.rotation.getUp(), -moveAmount)
 
-		# Mouse buttons
-		'''
-		if 'MOUSE_LEFT' in keys:
-			self.arcball_on = True
-		else:
-			self.arcball_on = False
-		'''
+        # Mouse buttons
+        '''
+        if 'MOUSE_LEFT' in keys:
+            self.arcball_on = True
+        else:
+            self.arcball_on = False
+        '''
 
-	def onMouseMove(self, deltaX, deltaY, tick):
-		if deltaX != 0:
-			self.rotate(self.yAxis, math.radians(deltaX * self.sensitivity * tick))
+    def onMouseMove(self, deltaX, deltaY, tick):
+        if deltaX != 0:
+            self.rotate(self.yAxis, math.radians(deltaX * self.sensitivity * tick))
 
-		if deltaY != 0:
-			self.rotate(self.rotation.getRight(), math.radians(-deltaY * self.sensitivity * tick))
+        if deltaY != 0:
+            self.rotate(self.rotation.getRight(), math.radians(-deltaY * self.sensitivity * tick))
 
-		if self.arcball_on:
-			self.cur_x = deltaX
-			self.cur_y = deltaY
-			self.doArcBallRotation()
+        if self.arcball_on:
+            self.cur_x = deltaX
+            self.cur_y = deltaY
+            self.doArcBallRotation()
 
-	def rotate(self, axis, angle):
-		self.rotation = self.rotation * quaternion.QuatFromAxis(axis, angle)
-		self.rotation.i_normalize()
+    def rotate(self, axis, angle):
+        self.rotation = self.rotation * quaternion.QuatFromAxis(axis, angle)
+        self.rotation.i_normalize()
 
-	def move(self, direction, amount):
-		self.position = self.position + (direction * amount)
+    def move(self, direction, amount):
+        self.position = self.position + (direction * amount)
 
-	def setPosition(self, position):
-		self.position = position
+    def setPosition(self, position):
+        self.position = position
 
-	def getViewMatrix(self):
-		self.calcViewMatrix()
-		return self.viewMatrix
+    def getViewMatrix(self):
+        self.calcViewMatrix()
+        return self.viewMatrix
 
-	def doArcBallRotation(self):
-		
-		def get_vector(x, y):
-			point = vector.Vector(3, data=[1.0 * x / self.screen_width * 2 - 1.0, -(1.0 * y / self.screen_height * 2 - 1.0), 0.0])
-			
-			pointSqred = point[0] * point[0] + point[1] * point[1]
+    def doArcBallRotation(self):
+        
+        def get_vector(x, y):
+            point = vector.Vector(3, data=[1.0 * x / self.screen_width * 2 - 1.0, -(1.0 * y / self.screen_height * 2 - 1.0), 0.0])
+            
+            pointSqred = point[0] * point[0] + point[1] * point[1]
 
-			if pointSqred <= 1 * 1:
-				point[2] = math.sqrt(1*1 - pointSqred)
-			else:
-				point.i_normalize()
+            if pointSqred <= 1 * 1:
+                point[2] = math.sqrt(1*1 - pointSqred)
+            else:
+                point.i_normalize()
 
-			return point
+            return point
 
-		if (self.cur_x is not last_x) or (self.cur_y is not last_y):
-			va = get_vector(last_x, last_y)
-			vb = get_vector(self.cur_x, self.cur_y)
+        if (self.cur_x is not last_x) or (self.cur_y is not last_y):
+            va = get_vector(last_x, last_y)
+            vb = get_vector(self.cur_x, self.cur_y)
 
-			angle = math.acos(math.min(1.0, va.dot(vb)))
+            angle = math.acos(math.min(1.0, va.dot(vb)))
 
-			axis_in_camera_coord = va.cross(vb)
+            axis_in_camera_coord = va.cross(vb)
 
-			self.rotate(axis_in_camera_coord, angle * self.sensitivity * tick)
+            self.rotate(axis_in_camera_coord, angle * self.sensitivity * tick)
 
-			last_x = self.cur_x
-			last_y = self.cur_y
-		
+            last_x = self.cur_x
+            last_y = self.cur_y
+        
 
 
