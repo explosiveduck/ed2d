89951bb1044a3682fb0d506fc39cd6407c38513e
Matthew Sitton <matthewsitton@gmail.com>
2015-09-27 12:46:51 -0500

    Attempt 2.

--- ed2d/debug.py
+++ ed2d/debug.py
@@ -1,7 +1,8 @@
+from __future__ import print_function
 from ed2d.cmdargs import CmdArgs
 
 debug = CmdArgs.add_arg('debug', bool, 'Enable debug output.')
 
 def _debug(*args):
     if debug:
-        print (*args)
+        print(*args)
