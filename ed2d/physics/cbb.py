dc30ce4940219a9cc74d4f04c8db39e122f90a31
Matthew Sitton <matthewsitton@gmail.com>
2015-09-27 03:48:06 -0500

    Fix tabs in cbb.py

--- ed2d/physics/cbb.py
+++ ed2d/physics/cbb.py
@@ -5,22 +5,22 @@ from ed2d.glmath import vector
 class CBB(object):
 
     def __init__(self, radius, center):
-    	'''Creates a circle bounding box object to be used with the physics engine. Takes in a float for the radius and an array for the center.'''
+        '''Creates a circle bounding box object to be used with the physics engine. Takes in a float for the radius and an array for the center.'''
         self.radius = radius
         self.center = vector.Vector(3, data=center)
 
     def intersectCBB(self, oCBB):
-    	tempDistance = self.center - oCBB.center
-    	distanceCenters = tempDistance.magnitude()
-    	distanceRadii = self.radius + oCBB.radius
+        tempDistance = self.center - oCBB.center
+        distanceCenters = tempDistance.magnitude()
+        distanceRadii = self.radius + oCBB.radius
 
-    	# Collision happens when the distance between the two centers is less than the sum of the radii
-    	state = distanceCenters < distanceRadii
+        # Collision happens when the distance between the two centers is less than the sum of the radii
+        state = distanceCenters < distanceRadii
 
-    	# Calculate the depth penetration
-    	depthPenetration = distanceCenters - (distanceRadii)
+        # Calculate the depth penetration
+        depthPenetration = distanceCenters - (distanceRadii)
 
-    	return CollisionData(state, tempDistance, depthPenetration)
+        return CollisionData(state, tempDistance, depthPenetration)
 
     def getCenter(self):
         return self.center
