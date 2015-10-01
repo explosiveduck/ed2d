21f18e61ec34203957a2a1fd968c5ca29cd7507d
Matthew Sitton <matthewsitton@gmail.com>
2015-09-30 21:27:20 -0500

    Fill out the placeholder a bit more.

--- ed2d/audio.py
+++ ed2d/audio.py
@@ -3,3 +3,18 @@ class Audio(object):
     """Stub class for for Audio code"""
     def __init__(self):
         pass
+
+    def play(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def get_pos(self):
+        pass
+
+    def volume(self, vol):
+        pass
