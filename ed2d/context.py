7fea38effcf89297fd5d08575308426650de7a44
Matthew Sitton <matthewsitton@gmail.com>
2015-10-01 00:02:21 -0500

    Implement changes on other platform files.

--- ed2d/context.py
+++ ed2d/context.py
@@ -8,11 +8,11 @@ osName = platform.system()
 # 	from ed2d.context.win32 import Context
 # # elif osName == 'Linux':
 # 	# from ed2d.context.x11 import Context
-# elif osName in ('Darwin', 'Linux'):
-# 	from ed2d.context.sdl2 import Context
-# else:
-# 	pass
+if osName in ('Windows', 'Darwin', 'Linux'):
+    from ed2d.platforms.context.sdl import Context
+else:
+    pass
 
-from ed2d.platforms.context.sdl import Context
+
 
 __all__ = ['Context']
--- ed2d/events.py
+++ ed2d/events.py
@@ -3,14 +3,14 @@ import platform
 # from ed2d.cmdargs import  CmdArgs
 # forceSDL = CmdArgs.add_arg('sdl', bool, 'Force sdl2 instead of native.')
 
-# osName = platform.system()
+osName = platform.system()
 # if osName == 'Windows':
-# 	from ed2d.events.win32 import Events
-# # elif osName == 'Linux':
-# 	# from ed2d.events.x11 import Events
-# elif osName in ('Darwin', 'Linux'):
-# 	from ed2d.events.sdl2 import Events
-# else:
-# 	pass
-from ed2d.platforms.events.sdl import Events
+#     from ed2d.events.win32 import Events
+# elif osName == 'Linux':
+#     from ed2d.events.x11 import Events
+if osName in ('Darwin', 'Linux'):
+    from ed2d.platforms.events.sdl import Events
+else:
+    pass
+
 __all__ = ['Events']
