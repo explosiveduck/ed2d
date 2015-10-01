648f908de4774eca6a528eb0007392556579e9db
Matthew Sitton <matthewsitton@gmail.com>
2015-09-30 22:13:11 -0500

    Uncomment some of the platform checking code.

--- ed2d/monitors.py
+++ ed2d/monitors.py
@@ -3,15 +3,15 @@ import platform
 # from ed2d.cmdargs import  CmdArgs
 # forceSDL = CmdArgs.add_arg('sdl', bool, 'Force sdl2 instead of native.')
 
-# osName = platform.system()
+osName = platform.system()
 # if osName == 'Windows':
 # 	from ed2d.monitors.win32 import Monitors
 # # elif osName == 'Linux':
 # 	# from ed2d.monitors.x11 import Monitors
-# elif osName in ('Darwin', 'Linux'):
-# 	from ed2d.monitors.sdl2 import Monitors
-# else:
-# 	pass
+if osName in ('Windows', 'Darwin', 'Linux'):
+ 	from ed2d.monitors.sdl2 import Monitors
+else:
+ 	pass
 
 from ed2d.platforms.monitors.sdl import Monitors
 
