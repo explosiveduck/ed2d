7a21f816bb21966e6142e05757eb0773a40fdaf8
Alex <ale632007@gmail.com>
2016-01-14 01:22:51 -0500

    Small update to objloader.

--- ed2d/assets/objloader.py
+++ ed2d/assets/objloader.py
@@ -1,4 +1,7 @@
 
+from ed2d.mesh import MeshBase
+from ed2d.assets.mtlloader import MTL
+
 class OBJ(object):
     def __init__(self, fileName):
         ''' Wavefront .obj file parser.'''
@@ -21,7 +24,7 @@ class OBJ(object):
                 vtcount += 1
             elif value == 'vn':
                 vncount += 1
-    
+
         fcount *= 3
         vcount *= 3
         vtcount *= 3
@@ -56,45 +59,45 @@ class OBJ(object):
     def __process_in_house(self, objfl):
         with open(filename, "r") as objfl:
 
-        for line in objfl:
+            for line in objfl:
 
-            value = line.split()
-            valueType = value[0]
+                value = line.split()
+                valueType = value[0]
 
-            if valueType not in ['f', 'v', 'vt', 'vn']:
-                continue
+                if valueType not in ['f', 'v', 'vt', 'vn']:
+                    continue
 
-            value = value[1:]
+                value = value[1:]
 
-            # Check first and continue on early because of string splitting
-            if valueType == "f":
-                temp = [item.split("/") for item in value]
-                    
-                for i in range(3):
-                    if temp[i][1] != '':
-                        self.uvIndices[fnumber] = int(temp[i][1])
-                    self.vertexIndices[fnumber] = int(temp[i][0])
-                    self.normalIndices[fnumber] = int(temp[i][2])
-                    fnumber += 1
+                # Check first and continue on early because of string splitting
+                if valueType == "f":
+                    temp = [item.split("/") for item in value]
 
-                continue
+                    for i in range(3):
+                        if temp[i][1] != '':
+                            self.uvIndices[fnumber] = int(temp[i][1])
+                        self.vertexIndices[fnumber] = int(temp[i][0])
+                        self.normalIndices[fnumber] = int(temp[i][2])
+                        fnumber += 1
 
-            value = list(map(float, value))
+                    continue
 
-            if valueType == "v":
-                v = [value[0], value[1], value[2]]
-                self.tempVertices[vnumber] = v
-                self.vnumber += 1
+                value = list(map(float, value))
 
-            elif valueType == "vt":
-                vt = [value[0], value[0]]
-                self.tempUVs[vtnumber] = vt
-                self.vtnumber += 1
+                if valueType == "v":
+                    v = [value[0], value[1], value[2]]
+                    self.tempVertices[vnumber] = v
+                    self.vnumber += 1
 
-            elif valueType == "vn":
-                n = [value[0], value[1], value[2]]
-                self.tempNormals[vnnumber] = n
-                self.vnnumber += 1
+                elif valueType == "vt":
+                    vt = [value[0], value[0]]
+                    self.tempUVs[vtnumber] = vt
+                    self.vtnumber += 1
+
+                elif valueType == "vn":
+                    n = [value[0], value[1], value[2]]
+                    self.tempNormals[vnnumber] = n
+                    self.vnnumber += 1
 
     def get_final_data(self):
         for i in range(fcount):
@@ -110,3 +113,7 @@ class OBJ(object):
                 uvIndex = self.uvIndices[i]
                 uv = self.tempUVs[int(uvIndex) - 1]
                 self.finalUVs[i] = uv
+
+class ObjMesh(MeshBase):
+    def __init__(self, filePath, name, program, vertexLoc, normalLoc):
+        super(ObjMesh, self).__init__(filePath, name, program, vertexLoc, normalLoc)
