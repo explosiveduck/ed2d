0dba88140f890c1b8d742455ddf208f251bab89c
Alex <ale632007@gmail.com>
2016-01-14 01:19:43 -0500

    Nuked glmath. Issue #27

--- ed2d/glmath/__init__.py
+++
@@ -1,4 +0,0 @@
-from ed2d.glmath.matrix import Matrix, orthographic
-from ed2d.glmath.vector import Vector
-
-__all__ = ['Matrix', 'Vector', 'orthographic']
--- ed2d/glmath/cython/__init__.py
+++
@@ -1,2 +0,0 @@
-from ed2d.glmath.cython.matrix import *
-from ed2d.glmath.cython.vector import *
