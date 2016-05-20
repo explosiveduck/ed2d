16e7a298a83fe53174bda8ec77dfcf6869ed5336
Matthew Sitton <matthewsitton@gmail.com>
2016-05-20 00:43:49 -0500

    Add html support to xml parser, rename to markup.py

--- ed2d/markup.py
+++ ed2d/markup.py
@@ -1,5 +1,5 @@
 from xml.parsers import expat
-
+from html.parser import HTMLParser
 
 class TagStack(object):
     def __init__(self):
@@ -21,6 +21,10 @@ class TagStack(object):
         self.data[self.stackSize-1].append(data)
         self.dataAdded[-1] = True
 
+    def clear_frame_data(self):
+        self.data[self.stackSize-1] = []
+        self.dataAdded[-1] = False
+
     def is_data_added(self, posRel=0):
         pos = -1 - posRel
         return self.dataAdded[pos]
@@ -40,8 +44,8 @@ class TagStack(object):
 
 
 class BaseParser(object):
-    def __init__(self, xmlParser, tag, parent, root):
-        self.xmlParser = xmlParser
+    def __init__(self, parser, tag, parent, root):
+        self.parser = parser
         self.parent = parent
         self.tag = tag
         self.root = root
@@ -61,16 +65,18 @@ class BaseParser(object):
 
         self.set_handlers()
 
+        self.init_data()
+
     def set_handlers(self):
-        self.xmlParser.StartElementHandler = self.start
-        self.xmlParser.CharacterDataHandler = self.data
-        self.xmlParser.EndElementHandler = self.end
+        pass
 
     def restore_handlers(self):
         if self.parent is not None:
             self.parent.set_handlers()
 
     def start(self, tag, attrs):
+        if not isinstance(attrs, dict):
+            attrs = dict(attrs)
 
         self.stack.push(tag, attrs)
 
@@ -83,11 +89,14 @@ class BaseParser(object):
                 self.parsers[parser]['instance'] = parInst
 
     def data(self, data):
+        # We need to check if the stack frame has been used
+        # previously and clear the previous data if so.
+        if self.stack.is_data_added() is True:
+            self.stack.clear_frame_data()
         self.stack.add_data(data.strip())
         self.parse()
 
     def end(self, tag):
-
         if self.stack.is_data_added() is False:
             self.parse()
 
@@ -97,20 +106,63 @@ class BaseParser(object):
 
         self.stack.pop()
 
+    def switch_parser(self, parser):
+        tag, attrs, data = self.stack.peek()
+        return parser(self.parser, tag, self, self.root)
+
+    def register_parser(self, stackTree, parser):
+        self.parsers[stackTree] = {'object': parser}
+
+    # The following method stubs are what the parsing sub-classes
+    # will be implemented within.
+    def init_data(self):
+        pass
+
     def parse(self):
         pass
 
     def integrate(self):
         pass
 
-    def switch_parser(self, parser):
-        tag, attrs, data = self.stack.peek()
-        return parser(self.xmlParser, tag, self, self.root)
+class XmlParser(BaseParser):
+    def set_handlers(self):
+        self.parser.StartElementHandler = self.start
+        self.parser.CharacterDataHandler = self.data
+        self.parser.EndElementHandler = self.end
 
-    def register_parser(self, stackTree, parser):
-        self.parsers[stackTree] = {'object': parser}
+class HtmlParser(BaseParser):
+    def set_handlers(self):
+        self.parser.handle_starttag = self.start
+        self.parser.handle_data = self.data
+        self.parser.handle_endtag = self.end
 
+def parse_html_data(rootParser, htmlData):
+    htmlParser = HTMLParser()
+    root = rootParser(htmlParser, None, None, None)
+    linedData = htmlData.split('\n')
+    for line in linedData:
+        htmlParser.feed(line.strip())
+    return root
 
+def parse_html(rootParser, htmlPath):
+    htmlParser = HTMLParser()
+    root = rootParser(htmlParser, None, None, None)
+
+    with open(htmlPath, 'rb') as htmlFile:
+        for line in htmlFile:
+            htmlParser.feed(line.strip())
+    
+    return root
+
+def parse_xml_data(rootParser, xmlData):
+    xmlParser = expat.ParserCreate()
+    root = rootParser(xmlParser, None, None, None)
+    linedData = xmlData.split('\n')
+    for line in linedData:
+        xmlParser.Parse(line.strip(), 0)
+
+    xmlParser.Parse(b'',)
+    return root
 
 def parse_xml(rootParser, xmlPath):
 
@@ -119,7 +171,6 @@ def parse_xml(rootParser, xmlPath):
 
     with open(xmlPath, 'rb') as xmlFile:
         for line in xmlFile:
-            # print(line.strip())
             xmlParser.Parse(line.strip(), 0)
 
     xmlParser.Parse(b'', 1)
